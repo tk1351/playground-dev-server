6bc993c4849cc01bcdab882d72381727d270aecb
tk1351 <takakikitsui0111@gmail.com>
2024-05-24 14:37:50 +0900

    feat: 拡張子の省略対応

--- micro-server/src/resolvePlugin.ts
+++ micro-server/src/resolvePlugin.ts
@@ -4,17 +4,21 @@ import type { Plugin } from "rollup";
 
 const root = process.cwd();
 
+const extensions = ["", ".ts", ".js"];
+
 export const resolve = (): Plugin => {
 	return {
 		name: "micro-server:resolve",
 		async resolveId(id: string) {
-			const absolutePath = path.resolve(root, `.${id}`);
-			try {
-				const stat = await fs.stat(absolutePath);
-				if (stat.isFile()) {
-					return absolutePath;
-				}
-			} catch {}
+			for (const extension of extensions) {
+				const absolutePath = path.resolve(root, `.${id}${extension}`);
+				try {
+					const stat = await fs.stat(absolutePath);
+					if (stat.isFile()) {
+						return absolutePath;
+					}
+				} catch {}
+			}
 			return null;
 		},
 		async load(id: string) {
