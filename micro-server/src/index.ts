84891738244fe2265043bc9c85bbef4a0fbc1771
tk1351 <takakikitsui0111@gmail.com>
2024-05-24 12:26:08 +0900

    feat: cli 作成

--- micro-server/src/index.ts
+++ micro-server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import cac from 'cac';
+
+const cli = cac();
+
+cli.command('dev')
+    .action(() => {
+        console.log('dev server start')
+    });
+cli.command('build')
+    .action(() => {
+        console.log('build start')
+    });
+
+cli.help();
+cli.parse();
